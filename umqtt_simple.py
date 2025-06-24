17f816a6f9abb1f3ae20d60db05ce6a833ed1f28
thuanhoang07 <zz.mr.thuan.zz@gmail.com>
2025-06-24 16:18:57 +0700

    v2

--- umqtt_simple.py
+++ umqtt_simple.py
@@ -62,65 +62,39 @@ class MQTTClient:
     self.lw_retain = retain
 
   def connect(self, clean_session=True):
-    import socket
-    from umqtt_simple import MQTTException
-
-    # Tạo socket, bật timeout
     self.sock = socket.socket()
-    self.sock.settimeout(5)                  # chờ tối đa 5 giây
     self.sock.connect(self.addr)
     if self.ssl:
-        import ussl
-        self.sock = ussl.wrap_socket(self.sock, **self.ssl_params)
-
-    # Build CONNECT packet...
+      import ussl
+      self.sock = ussl.wrap_socket(self.sock, **self.ssl_params)
     msg = bytearray(b"\x10\0\0\x04MQTT\x04\x02\0\0")
     msg[1] = 10 + 2 + len(self.client_id)
     msg[9] = clean_session << 1
     if self.user is not None:
-        msg[1] += 2 + len(self.user) + 2 + len(self.pswd)
-        msg[9] |= 0xC0
+      msg[1] += 2 + len(self.user) + 2 + len(self.pswd)
+      msg[9] |= 0xC0
     if self.keepalive:
-        assert self.keepalive < 65536
-        msg[10] |= self.keepalive >> 8
-        msg[11] |= self.keepalive & 0xFF
+      assert self.keepalive < 65536
+      msg[10] |= self.keepalive >> 8
+      msg[11] |= self.keepalive & 0x00FF
     if self.lw_topic:
-        msg[1] += 2 + len(self.lw_topic) + 2 + len(self.lw_msg)
-        msg[9] |= 0x04 | ((self.lw_qos & 1) << 3) | ((self.lw_qos & 2) << 3)
-        msg[9] |= self.lw_retain << 5
-
+      msg[1] += 2 + len(self.lw_topic) + 2 + len(self.lw_msg)
+      msg[9] |= 0x4 | (self.lw_qos & 0x1) << 3 | (self.lw_qos & 0x2) << 3
+      msg[9] |= self.lw_retain << 5
     self.sock.write(msg)
+    #print(hex(len(msg)), hexlify(msg, ":"))
     self._send_str(self.client_id)
     if self.lw_topic:
-        self._send_str(self.lw_topic)
-        self._send_str(self.lw_msg)
+      self._send_str(self.lw_topic)
+      self._send_str(self.lw_msg)
     if self.user is not None:
-        self._send_str(self.user)
-        self._send_str(self.pswd)
-
-    # Helper: đọc đúng n byte bằng recv()
-    def _read_exact(n):
-        buf = b''
-        while len(buf) < n:
-            try:
-                chunk = self.sock.recv(n - len(buf))
-            except OSError as e:
-                raise MQTTException("Socket recv error: %s" % e)
-            if not chunk:
-                raise MQTTException("CONNECT failed: incomplete CONNACK")
-            # debug: in ra độ dài chunk
-            print("recv chunk len:", len(chunk))
-            buf += chunk
-        return buf
-
-    resp = _read_exact(4)
-    print("CONNACK raw:", resp)
-    if not (resp[0] == 0x20 and resp[1] == 0x02):
-        raise MQTTException("Bad CONNACK header: %r" % resp)
+      self._send_str(self.user)
+      self._send_str(self.pswd)
+    resp = self.sock.read(4)
+    assert resp[0] == 0x20 and resp[1] == 0x02
     if resp[3] != 0:
-        raise MQTTException("CONNACK return code %d" % resp[3])
+      raise MQTTException(resp[3])
     return resp[2] & 1
-
 
   def disconnect(self):
     self.sock.write(b"\xe0\0")
