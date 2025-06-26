d27128bbb13e232e5caedf3a43a95ddbebf679a5
thuanhoang07 <zz.mr.thuan.zz@gmail.com>
2025-06-26 16:30:29 +0700

    v9

--- mqtt.py
+++ mqtt.py
@@ -83,7 +83,7 @@ class MQTT:
             pass
         self.client.connect()
         self.client.set_callback(self.__on_receive_message)
-        say('Connected to MQTT broker---------------------------v7')
+        say('Connected to MQTT broker---------------------------v9')
 
         # 2) Chỉ publish "online" thôi
         online_topic   = f"eoh/chip/{username}/is_online"
