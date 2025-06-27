87af7bf72232761a19348d58d5896a745955ab39
thuanhoang07 <zz.mr.thuan.zz@gmail.com>
2025-06-27 15:36:16 +0700

    \n

--- definition.js
+++ definition.js
@@ -68,7 +68,7 @@ Blockly.Python['yolobit_mqtt_connect_default_servers'] = function(block) {
 
   // Code connect và subscribe-down luôn trong cùng 1 chỗ (tuỳ chọn)
   var code  = `mqtt.connect_broker(server='${server}', username=${username}, password=${key})\n`;
-      code += 'time.sleep(5)'; // Đợi 5 giây để kết nối ổn định
+      code += 'time.sleep(5)\n'; // Đợi 5 giây để kết nối ổn định
       code += 'mqtt.subscribe_config_down(TOKEN)\n';
       code += 'for pin, cfg in mqtt.virtual_pins.items():\n';
       code += '    print("Virtual pin V%d → config_id %d" % (pin, cfg))\n';
